52bf8a81baf0a0b632251a0c41e5e6567170c36b
Turið Højgaard <tuho@itu.dk>
2022-09-04 21:30:04 +0200

    Lavet første udgave af LeapYear klassen

---
+++ MyApp/MyApp/LeapYear.cs
@@ -0,0 +1,18 @@
+// See https://aka.ms/new-console-template for more information
+
+namespace MyApp;
+
+//true
+// divideres med 4
+// ikke med 100, medmindre de kan divideres med 400
+
+public class LeapYear {
+    
+    public static void main(string[] args) {
+
+    }
+    public bool IsALeapYear(int year) {
+    return true;
+    }
+
+}
