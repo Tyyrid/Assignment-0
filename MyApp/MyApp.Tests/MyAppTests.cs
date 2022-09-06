dfe428c96ec1c8250fdd3c7a0ff372361a165549
Turið Højgaard <tuho@itu.dk>
2022-09-06 12:55:16 +0200

    Created user interface for IsLeapYear.

--- MyApp/MyApp.Tests/MyAppTests.cs
+++ MyApp/MyApp.Tests/MyAppTests.cs
@@ -16,6 +16,7 @@ public class MyAppTests
         result.Should().Be(true);
     }
 
+
     [Fact]
         public void Checks_if_year_1700_is_leap_year()
     {
@@ -29,29 +30,31 @@ public class MyAppTests
         result.Should().Be(false);
     }
 
+
+
         [Fact]
         public void Checks_if_user_gets_yay_feedback()
     {
         //Arrange//
         var sut = new LeapYear();
-        var feedback = "";
+        //var feedback = "";
         using var writer = new StringWriter();
-        TextWriter originalOutput = Console.Out; //laver ikke noget..
-        //Console.SetOut(writer);
+        Console.SetOut(writer);
 
-        /*var textReader = new StringReader("2000");
-        Console.SetIn(textReader);*/
+        var textReader = new StringReader("2000");
+        Console.SetIn(textReader);
 	    
 
         //Act//
         sut.isLeapYearWithUserInput();
 
         //Assert//
-        //var output = writer.GetStringBuilder();
-        //var lines = output.ToString().Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+        var output = writer.GetStringBuilder();
+        var lines = output.ToString().Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+        Assert.Equal("yay", lines[1]);
 
-        var outputLines = writer.ToString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-        
+
+        /*var outputLines = writer.ToString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
         if(String.Equals(outputLines[2], "yay")) {
             feedback = "yay";
         } else {
@@ -59,9 +62,8 @@ public class MyAppTests
         }
         Assert.Equal("Enter year: ", outputLines[0]); //Sammenligner det som console skriver (men som bruger kan man ikke se det længere)
         Assert.Equal(outputLines[1], outputLines[1]); //Sammenligner input frá bruger
-        Assert.Equal(feedback, outputLines[2]); //Sammenligner feedback ("yay" eller "nay")
+        Assert.Equal(feedback, outputLines[2]); //Sammenligner feedback ("yay" eller "nay")*/
         
-        //Assert.Equal("yay", lines[1]);
     }
   
 }
--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MyApp;
+
 Console.WriteLine("Hello, World!");
-    
+
+var ly = new LeapYear();
+ly.isLeapYear(1600);
+ly.isLeapYearWithUserInput();
